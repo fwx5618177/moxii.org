b0f797c2819dce98b635da42befd873720996bcd
fwx5618177 <fengwenxuan2006@126.com>
2023-12-11 01:26:08 +0800

    feat: update the toc status

--- src/reducers/toc/initialTocState.ts
+++ src/reducers/toc/initialTocState.ts
@@ -2,4 +2,7 @@ import { TocState } from "Reducer";
 
 export const initialTocState: TocState = {
   toc: [],
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
 };
--- src/types/Reducer.d.ts
+++ src/types/Reducer.d.ts
@@ -4,6 +4,9 @@ declare module "Reducer" {
 
   type TocState = {
     toc: TocTypes[];
+    isLoading: boolean;
+    isError: boolean;
+    isSuccess: boolean;
   };
 
   type ActionTypes =
