09ba56081f82328be0a782e7588a44d1be13cfa1
fwx5618177 <fengwenxuan2006@126.com>
2023-12-25 14:07:56 +0800

    feat: add new dash

--- src/layout/DashBoardLayout.tsx
+++ src/layout/DashBoardLayout.tsx
@@ -2,9 +2,11 @@
 
 import { DashboardLayoutProps } from "Components";
 import React, { FC, useState } from "react";
+import { usePathname } from "next/navigation";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
-import { Layout, Menu, Button, theme } from "antd";
+import { Layout, Menu, Button, theme, Breadcrumb } from "antd";
 import { SideItem } from "@/settings/side.config";
+import { useAuthContext } from "@/contexts/AuthContext";
 
 const { Header, Sider, Content } = Layout;
 
@@ -13,6 +15,8 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const uriParams = usePathname();
+  const { logout } = useAuthContext();
 
   return (
     <Layout
@@ -25,7 +29,9 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
         <Menu theme="dark" mode="inline" items={SideItem} />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header
+          style={{ padding: 0, background: colorBgContainer, display: "flex" }}
+        >
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
@@ -36,7 +42,34 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
               height: 64,
             }}
           />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button
+              type="primary"
+              onClick={logout}
+              style={{
+                marginRight: 12,
+              }}
+            >
+              Logout
+            </Button>
+          </div>
         </Header>
+
+        {uriParams && (
+          <Breadcrumb style={{ margin: 16 }}>
+            {uriParams?.split("/")?.map((item, index) => {
+              if (!!item)
+                return <Breadcrumb.Item key={index}>{item}</Breadcrumb.Item>;
+            })}
+          </Breadcrumb>
+        )}
+
         <Content
           style={{
             margin: "24px 16px",
--- src/views/DashBoard/DashBoardRootView.tsx
+++ src/views/DashBoard/DashBoardRootView.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import { useAuthContext } from "@/contexts/AuthContext";
-import { Button } from "antd";
-
 const DashBoardRootView = () => {
-  const { logout } = useAuthContext();
-
   return (
     <div>
       <h1>Dashboard</h1>
-      <Button type="primary" onClick={logout}>
-        Logout
-      </Button>
     </div>
   );
 };
